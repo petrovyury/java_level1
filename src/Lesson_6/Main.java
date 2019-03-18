bd6dfdb144adf83d5f1928f8c3f9d6116e9d87a9
petrovyuriy <petrov.0.yury@gmail.com>
2019-03-18 17:22:35 +0300

    HomeWork for lesson 6 plus String

--- src/Lesson_6/Main.java
+++ src/Lesson_6/Main.java
@@ -25,5 +25,34 @@ public class Main {
 
         dog1.swim(17);
         dog2.swim(14);
+
+        System.out.println();
+
+        String s = "Предложение  один     Теперь предложение    два   Предложение   три";
+        String s1 = s.replaceAll(" +", " ");
+        StringBuilder s2 = new StringBuilder(s1);
+
+        for (int i = 1; i < s1.length(); i++) {
+
+            if(s1.charAt(i) >= 'А' && s1.charAt(i) <= 'Я') {
+                s2.setCharAt(i-1, '.');
+
+            }
+
+
+        }
+
+        for (int i = 1; i < s2.length(); i++) {
+
+            if (s2.charAt(i) == '.') {
+                s2.insert(i+1," ");
+            }
+        }
+
+        s2.insert(s2.length(), '.');
+
+
+        System.out.println(s2);
+
     }
 }
